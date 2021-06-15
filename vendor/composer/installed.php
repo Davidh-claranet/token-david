9a28a63e0760102883199ca6b75f6845984d0904
David Hautbois <david.hautbois@fr.clara.net>
2021-06-15 21:38:11 +0200

    --

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-8.x-2.2',
+        'version' => 'dev-8.x-2.2',
+        'type' => 'drupal-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '17d9d14d0143bd78721273913c1f032d69c9594d',
+        'name' => 'david/token',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'david/token' => array(
+            'pretty_version' => 'dev-8.x-2.2',
+            'version' => 'dev-8.x-2.2',
+            'type' => 'drupal-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '17d9d14d0143bd78721273913c1f032d69c9594d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
